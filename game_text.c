5060672d5073f35441286563ea69f2eb8c9d9a4f
Justine <justine.ruch@etu.u-bordeaux.fr>
2022-10-19 12:10:53 +0200

    modif print

--- game_text.c
+++ game_text.c
@@ -1,10 +1,11 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "game.h"
 #include "game_aux.h"
+#define DEFAULT_SIZE 6
 
-void help(void)
-{
+void help(void){
     printf("> action : help\n");
     printf("-press 'w <i> <j>' to put a zero/white at square (i,j)\n");
     printf("-press 'b <i> <j>' to put a one/black at square (i,j)\n");
@@ -13,86 +14,68 @@ void help(void)
     printf("-press 'q' to quit \n");
 }
 
-int main(void)
-{
-    game g = game_default();
-    uint i = 0;
-    uint j = 0;
-    uint size = DEFAULT_SIZE;
-    game_play_move(g, i, j, S_ZERO);
-    while (game_is_over(g) == false)
-    {
+int main(void){
+    game g=game_default();
+    while (game_is_over(g)!=true){
         game_print(g);
-        for (int a = 0; a < size; a++)
-        {
-            for (int b = 0; b < size; b++)
-            {
-                int v = game_has_error(g, a, b);
-                if (v != 0)
-                {
-                    printf("error at case %d %d \n", a,b);
+        printf("> ? [h for help]\n");
+        for(uint i=0;i<DEFAULT_SIZE;i++){
+            for(uint j=0;j<DEFAULT_SIZE;j++){
+                if((game_has_error(g,i,j)!=0)){
+                    printf("Error at square(%u,%u)\n",i,j);
                 }
             }
         }
         char charc;
-        int retour = scanf(" %c", &charc);
-        if (retour != 1)
-        {
+        int retour=scanf(" %c",&charc);
+        if (retour!=1){
             help();
         }
-        else if (charc == 'h'){
+        else if(charc=='h'){
             help();
         }
-
-        else if (charc == 'r')
-        {
+        else if(charc=='r'){
+            printf("> action : restart\n");
             game_restart(g);
         }
-        else if (charc == 'q')
-        {
+        else if(charc=='q'){
+            printf("> action : quit\n");
+            printf("shame\n");
             game_delete(g);
-            printf("shame");
-            return (EXIT_SUCCESS);
+            return EXIT_SUCCESS;
         }
-        else if (charc == 'w' || charc == 'b' || charc == 'e')
-        {
-            int i, j;
-            int entiers = scanf(" %d %d", &i, &j);
-            if (entiers != 2)
-            {
+        else if(charc=='w'||charc=='b'||charc=='e'){
+            uint it, jt;
+            square jouer;
+            int retourbis = scanf("%u %u",&it,&jt);
+            if (retourbis!=2){
                 help();
             }
-            square move;
-        
-            if (charc == 'w')
-            {
-                printf("> action : help\n");
-                move = S_ZERO;
-                if (game_check_move(g, i, j, move) == true)
-                {
-                    game_play_move(g, i, j, move);
+            else if(charc=='w'){
+                jouer=S_ZERO;
+                if(game_check_move(g,it,jt,jouer)==true){
+                    printf("> action : play move '%c' into square (%u,%u)\n",charc,it,jt);
+                    game_play_move(g,it,jt,jouer);
                 }
             }
-            else if (charc == 'b')
-            {
-                move = S_ONE;
-                if (game_check_move(g, i, j, move) == true)
-                {
-                    game_play_move(g, i, j, move);
+            else if(charc=='b'){
+                jouer=S_ONE;
+                if(game_check_move(g,it,jt,jouer)==true){
+                    printf("> action : play move '%c' into square (%u,%u)\n",charc,it,jt);
+                    game_play_move(g,it,jt,jouer);
                 }
             }
-            else if (charc == 'e')
-            {
-                move = S_EMPTY;
-                if (game_check_move(g, i, j, move) == true)
-                {
-                    game_play_move(g, i, j, move);
+            else if(charc=='e'){
+                jouer=S_EMPTY;
+                if(game_check_move(g,it,jt,jouer)==true){
+                    game_play_move(g,it,jt,jouer);
                 }
             }
         }
-    }
 
-    printf("congratulations");
+    }
+    game_print(g);
+    printf("congratulation\n");
     game_delete(g);
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
