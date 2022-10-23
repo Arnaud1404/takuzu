f39d25ae559f9736f003388cbf44cac76b24ccd8
Arnaud Gomes <arnaud.gomes@etu.u-bordeaux.fr>
2022-10-23 14:42:05 +0200

    commit TD5 game_text_argomes, ajout test CMakeLists

--- game_text_argomes.c
+++ game_text_argomes.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]){
         return EXIT_FAILURE;
     }
     if (strcmp("dummy",argv[1])==0){
-        return EXIT_SUCCESS;
+        return test_dummy();
     }
     return EXIT_FAILURE;
 }
