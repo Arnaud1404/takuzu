81eccd3ad24941f6e7af6651b786df337d9f3f05
ymaabout <yasmine.maabout@etu.u-bordeaux.fr>
2022-11-02 14:59:50 +0100

    ajout de game is over test

--- game_test_ymaabout.c
+++ game_test_ymaabout.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include "game.h"
+#include "game_aux.h"
 
 
 bool test_dummy(void){
@@ -13,6 +15,19 @@ void usage(int argc, char *argv[])
   fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
   exit(EXIT_FAILURE);
 }
+
+int test_is_over(void){
+    game g = game_default_solution();
+    game g1 = game_default();
+    bool test = game_is_over(g);
+    bool test1 = !game_is_over(g1);
+    if (test && test1){
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+
+}
+
 
 
 int main(int argc, char* argv[]){
