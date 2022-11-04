3b7c8bb9f21f98227b07ca8f3eadb849f424d46c
Justine <justine.ruch@etu.u-bordeaux.fr>
2022-11-04 11:55:40 +0100

    test game solution default

--- game_test_juruch.c
+++ game_test_juruch.c
@@ -43,6 +43,51 @@ bool test_game_default{
     return true;
 }
 
+bool test_game_default_solution{
+    game g=game_new_empty();
+    game def=test_game_default_solution();
+    game_set_square(g, 0, 0, S_ZERO);
+    game_set_square(g, 0, 1, S_IMMUTABLE_ONE);
+    game_set_square(g, 0, 2, S_IMMUTABLE_ZERO);
+    game_set_square(g, 0, 3, S_ONE);
+    game_set_square(g, 0, 4, S_ZERO);
+    game_set_square(g, 0, 5, S_ONE);
+    game_set_square(g, 1, 0, S_ZERO);
+    game_set_square(g, 1, 1, S_ONE);
+    game_set_square(g, 1, 2, S_ONE);
+    game_set_square(g, 1, 3, S_ZERO);
+    game_set_square(g, 1, 4, S_ONE);
+    game_set_square(g, 1, 5, S_ZERO);
+    game_set_square(g, 2, 0, S_ONE);
+    game_set_square(g, 2, 1, S_IMMUTABLE_ZERO);
+    game_set_square(g, 2, 2, S_ZERO);
+    game_set_square(g, 2, 3, S_ONE);
+    game_set_square(g, 2, 4, S_IMMUTABLE_ZERO);
+    game_set_square(g, 2, 5, S_ONE);
+    game_set_square(g, 3, 0, S_ONE);
+    game_set_square(g, 3, 1, S_IMMUTABLE_ZERO);
+    game_set_square(g, 3, 2, S_IMMUTABLE_ONE);
+    game_set_square(g, 3, 3, S_ZERO);
+    game_set_square(g, 3, 4, S_ZERO);
+    game_set_square(g, 3, 5, S_ONE);
+    game_set_square(g, 4, 0, S_ZERO);
+    game_set_square(g, 4, 1, S_ONE);
+    game_set_square(g, 4, 2, S_IMMUTABLE_ONE);
+    game_set_square(g, 4, 3, S_ZERO);
+    game_set_square(g, 4, 4, S_ONE);
+    game_set_square(g, 4, 5, S_IMMUTABLE_ZERO);
+    game_set_square(g, 5, 0, S_ONE);
+    game_set_square(g, 5, 1, S_ZERO);
+    game_set_square(g, 5, 2, S_ZERO);
+    game_set_square(g, 5, 3, S_ONE);
+    game_set_square(g, 5, 4, S_ONE)
+    game_set_square(g, 5, 5, S_IMMUTABLE_ZERO);
+    if(game_equal(g, def)!=true){
+        return false;
+    }
+    return true;
+}
+
 int main(int argcount, char *argvalue[]){
     if(argcount==1){
         return EXIT_FAILURE;
