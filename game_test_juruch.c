e64c16fd8fbfecfb9b90d0fe6912daf94acb9215
Justine <justine.ruch@etu.u-bordeaux.fr>
2022-11-04 10:49:37 +0100

    début test new empty

--- game_test_juruch.c
+++ game_test_juruch.c
@@ -1,8 +1,24 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "game.h"
+#include "game_aux.h"
 
 bool test_dummy(){
     return EXIT_SUCCESS;
+}
+
+bool test_game_new_empty(){
+    game g=game_new_empty();
+    game def=game_default(); //vérifier que la taille de new empty est celle de default
+    for(uint i=0;i<DEFAULT_SIZE;i++){
+        for(uint j=0;j<DEFAULT_SIZE;j++){
+            if(game_is_empty(g, i, j)!=true)
+            return false;
+        }
+    }
+return true;
 }
 
 int main(int argcount, char *argvalue[]){
