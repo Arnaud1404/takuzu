cb7801dea99460a93e404a56f00410e286425b4e
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2022-11-04 14:19:50 +0100

    2 tests marchenttttt

--- game_test_ymaabout.c
+++ game_test_ymaabout.c
@@ -16,49 +16,52 @@ void usage(int argc, char *argv[])
   exit(EXIT_FAILURE);
 }
 
-int test_is_over(void){
+bool test_is_over(void){
     game g = game_default_solution();
     game g1 = game_default();
     bool test = game_is_over(g);
     bool test1 = !game_is_over(g1);
-    while(test1){
+    
         for(int i = 0;i<DEFAULT_SIZE;i++){
             for(int j = 0; j <DEFAULT_SIZE;j++){
-                 (if i !=4&& j !=2){  //si on remplit cette case, game est vraiment over 
-              if(game_get_number(g1,i,j)==-1){  //vérifie si la case est vide
+                if(i!= 5 && j!=3){
+              if(game_get_square(g1,i,j)!= S_IMMUTABLE_ONE && game_get_square(g1,i,j)!= S_IMMUTABLE_ONE){  //vérifie si la case est immutable
 
              
-            g1 = game_set_square(g1,i,j,game_get_square(g,i,j));
+            
             test1 = !game_is_over(g1);
-             }
+            if (test1 == false){
+                return false;
+            }
+            game_set_square(g1,i,j,game_get_square(g,i,j));}
             }
             }
         }
         
-    }
+
     game_delete(g);
     game_delete(g1);
-    if (test && test1){
-        return EXIT_SUCCESS;
+    if (test==1 && test1==1){
+        return true;
     }
-    return EXIT_FAILURE;
+    return false;
 
 }
 
 
 
-int test_game_restart(void){
+bool test_game_restart(void){
     game g = game_default();
-    game g2 = game_default()
-    g = game_play_move(g,0,0,S_ONE);
-    g = gaùe_play_move(g,0,1,S_ZERO);
-    g = game_restart()
-    for(int i = 0;i<DEFAULT_SIZE;i++){
-            for(int j = 0; j <DEFAULT_SIZE;j++){
-                if(game_get_square(g,i,j)!=game_get_square(g2,i,j)){
-                    return EXIT_FAILURE;
-                }
-            }} return EXIT_SUCCESS;
+    game g2 = game_default_solution();
+    game_restart(g2);
+    if(game_equal(g,g2)==true){
+        game_delete(g);
+        game_delete(g2);
+        return true;
+    }
+    game_delete(g);
+        game_delete(g2);
+    return false;
 }
 
 
