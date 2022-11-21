ff2ccd6e7472f5487792aa65f8855eac3887d4a8
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2022-11-21 10:29:44 +0100

    ajout de game_print

--- game_aux.c
+++ game_aux.c
@@ -20,7 +20,39 @@
  * @param g the game
  * @pre @p g must be a valid pointer toward a game structure.
  **/
-void game_print(cgame g){}
+void game_print(cgame g){
+    if(g == NULL){
+        exit(EXIT_FAILURE);
+    }
+    printf("   ");
+    for(int j = 0; j <DEFAULT_SIZE;j ++){
+        printf("%d",j);
+    }
+    printf("\n");
+    printf("  ------ ");
+
+    for(int i = 0; i < DEFAULT_SIZE ; i++){
+        printf("%d |",i);
+        for(int j = 0; j < DEFAUlt_SIZE;j++){
+            if(game_get_square(g,i,j)==S_EMPTY){
+                printf(" ");
+            }
+            if(game_get_square(g,i,j)==S_ZERO){
+                printf("w");
+            }
+            if(game_get_square(g,i,j)==S_ONE){
+                printf("b");
+            }
+            if(game_get_square(g,i,j)==S_IMMUTABLE_ONE){
+                printf("B");
+            }
+            if(game_get_square(g,i,j)==S_IMMUTABLE_ZERO){
+                printf("W");
+            }
+            
+        }printf("|\n");
+    }printf("  ------ \n");
+}
 
 /**
  * @brief Creates the default game.
