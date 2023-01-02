df4d3c354e2b82d0fda93a0da2a2b1b28740cd7f
Justine Ruch <justine.ruch@u-bordeaux.fr>
2023-01-02 16:53:04 +0100

    modif has error

--- game.c
+++ game.c
@@ -601,7 +601,7 @@ bool game_is_over(cgame g)
   if (g->uni == true) {
     for (uint lref = 0; lref < g->row; lref++) {
       int same = 0;
-      for (uint l = 0; l < g->row; l++) {
+      for (uint l = lref+1; l < g->row; l++) {
         for (uint c = 0; c < g->col; c++) {
           if (game_get_square(g, lref, c) == game_get_square(g, l, c)) {
             same = same + 1;
@@ -614,7 +614,7 @@ bool game_is_over(cgame g)
     }
     for (uint cref = 0; cref < g->col; cref++) {
       int same = 0;
-      for (uint c = 0; c < g->col; c++) {
+      for (uint c = cref+1; c < g->col; c++) {
         for (uint l = 0; l < g->row; l++) {
           if (game_get_square(g, l, cref) == game_get_square(g, l, c)) {
             same = same + 1;
