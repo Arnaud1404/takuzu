3dd6f02cc1d34edaae68d6826ee4274951b3551c
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2023-01-12 14:24:54 +0100

    ajout d'un test

--- game_test_ymaabout.c
+++ game_test_ymaabout.c
@@ -114,9 +114,9 @@ int test_game_print(void)
 int test_is_wrapping(void){
   game g = game_default();
   if(game_is_wrapping(g) == true){
-    return false;
+    return EXIT_FAILURE;
   }
-  return true;
+  return EXIT_SUCCESS;
 }
 int main(int argcount, char* argv[])
 {
