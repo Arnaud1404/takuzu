f770a66164b5cfcc08e1e5ebbfaa1889b0797956
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2023-01-14 15:39:03 +0100

    testttttt redo

--- game_test_argomes.c
+++ game_test_argomes.c
@@ -409,21 +409,43 @@ int test_game_undo()
   return EXIT_SUCCESS;
 }
 
-int test_game_redo()
+bool test_game_redo(void)
 {
-  game g = game_new_empty_ext(8, 4, true, true);
-  game_play_move(g, 0, 0, S_ZERO);
-  game_play_move(g,1,0,S_ONE);
-  game_undo(g);
-  game_redo(g);
+    game new_game = game_new_empty_ext(DEFAULT_SIZE, 8, true, true);
+    game_play_move(new_game, 3, 2, S_ONE);
+    game_undo(new_game);
+    game_redo(new_game);
+    game_print(new_game);
+    /* Je vérfie que l'historique des coups joués n'est pas vide et que la valeur dans
+    le head est bien le coup qui a été rejoué */
 
-  if (game_get_square(g, 1, 0) != S_ONE ) {
-    game_delete(g);
-    return EXIT_FAILURE;
-  }
-  game_delete(g);
-  return EXIT_SUCCESS;
+    if (game_get_square(new_game, 3, 2) != S_ONE) {
+        game_delete(new_game);
+        return false;
+    }
+
+    // On test de redo plusieurs coups
+    game_play_move(new_game, 3, 2, S_ONE);
+    game_play_move(new_game, 3, 1, S_ONE);
+    game_play_move(new_game, 1, 2, S_ZERO);
+    game_undo(new_game);
+    game_undo(new_game);
+    game_undo(new_game);
+    game_redo(new_game);
+    game_redo(new_game);
+    game_redo(new_game);
+
+    if (game_get_square(new_game, 3, 2) != S_ONE || game_get_square(new_game, 3, 1) != S_ONE ||
+        game_get_square(new_game, 1, 2) != S_ZERO) {
+        game_delete(new_game);
+        return false;
+    }
+
+    game_delete(new_game);
+
+    return true;
 }
+
 int main(int argcount, char* argv[])
 {
   int test;
