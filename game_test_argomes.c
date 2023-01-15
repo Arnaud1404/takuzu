d70d6167f61de16d3422461dc294ce63bb62229b
Justine Ruch <justine.ruch@u-bordeaux.fr>
2023-01-15 15:43:05 +0100

    Revert "essaie"
    This reverts commit 6705858ca7636a0eabd675423cdce28930df8a98

--- game_test_argomes.c
+++ game_test_argomes.c
@@ -14,56 +14,56 @@
 
 int test_dummy(void) { return EXIT_SUCCESS; }
 
-// int test_game_has_error(void)
-// {
-//   game g = game_default();
-//   game_set_square(g, 0, 0, S_ONE);        // is valid
-//   bool test1 = !game_has_error(g, 0, 0);  // EXIT_FAILURE
+int test_game_has_error(void)
+{
+  game g = game_default();
+  game_set_square(g, 0, 0, S_ONE);        // is valid
+  bool test1 = !game_has_error(g, 0, 0);  // EXIT_FAILURE
 
-//   game_set_square(g, 5, 2, S_ONE);       // is an error (3 black squares column)
-//   bool test2 = game_has_error(g, 5, 2);  // EXIT_SUCCESS
-//   game_set_square(g, 5, 2, S_EMPTY);
-//   bool test3 = !game_has_error(g, 5, 2);  // EXIT_FAILURE
+  game_set_square(g, 5, 2, S_ONE);       // is an error (3 black squares column)
+  bool test2 = game_has_error(g, 5, 2);  // EXIT_SUCCESS
+  game_set_square(g, 5, 2, S_EMPTY);
+  bool test3 = !game_has_error(g, 5, 2);  // EXIT_FAILURE
 
-//   game_set_square(g, 0, 3, S_ONE);        // is valid
-//   bool test4 = !game_has_error(g, 0, 3);  // EXIT_FAILURE
+  game_set_square(g, 0, 3, S_ONE);        // is valid
+  bool test4 = !game_has_error(g, 0, 3);  // EXIT_FAILURE
 
-//   game_set_square(g, 0, 4, S_ONE);       // is an error (too many black squares row)
-//   bool test5 = game_has_error(g, 0, 4);  // EXIT_SUCCESS
-//   game_set_square(g, 0, 4, S_EMPTY);
-//   bool test6 = !game_has_error(g, 0, 4);  // EXIT_FAILURE
+  game_set_square(g, 0, 4, S_ONE);       // is an error (too many black squares row)
+  bool test5 = game_has_error(g, 0, 4);  // EXIT_SUCCESS
+  game_set_square(g, 0, 4, S_EMPTY);
+  bool test6 = !game_has_error(g, 0, 4);  // EXIT_FAILURE
 
-//   game_set_square(g, 5, 4, S_ZERO);
-//   game_set_square(g, 5, 3, S_ZERO);        // 3 white in a row
-//   bool test7 = game_has_error(g, 5, 3);    // EXIT_SUCCESS
-//   bool test8 = game_has_error(g, 5, 4);    // EXIT_SUCCESS
-//   game_set_square(g, 5, 4, S_EMPTY);       // removes error
-//   bool test9 = !game_has_error(g, 5, 3);   // EXIT_FAILURE
-//   bool test10 = !game_has_error(g, 5, 4);  // EXIT_FAILURE
+  game_set_square(g, 5, 4, S_ZERO);
+  game_set_square(g, 5, 3, S_ZERO);        // 3 white in a row
+  bool test7 = game_has_error(g, 5, 3);    // EXIT_SUCCESS
+  bool test8 = game_has_error(g, 5, 4);    // EXIT_SUCCESS
+  game_set_square(g, 5, 4, S_EMPTY);       // removes error
+  bool test9 = !game_has_error(g, 5, 3);   // EXIT_FAILURE
+  bool test10 = !game_has_error(g, 5, 4);  // EXIT_FAILURE
 
-//   game_set_square(g, 1, 0, S_ONE);
-//   game_set_square(g, 3, 0, S_ONE);
-//   game_set_square(g, 4, 0, S_ONE);        // too many black squares (column)
-//   bool test11 = game_has_error(g, 4, 0);  // EXIT_SUCCESS
-//   game_set_square(g, 3, 0, S_EMPTY);
-//   bool test12 = !game_has_error(g, 4, 0);  // EXIT_FAILURE
+  game_set_square(g, 1, 0, S_ONE);
+  game_set_square(g, 3, 0, S_ONE);
+  game_set_square(g, 4, 0, S_ONE);        // too many black squares (column)
+  bool test11 = game_has_error(g, 4, 0);  // EXIT_SUCCESS
+  game_set_square(g, 3, 0, S_EMPTY);
+  bool test12 = !game_has_error(g, 4, 0);  // EXIT_FAILURE
 
-//   game_delete(g);
-//   if (!test1 || !test2 || !test3 || !test4 || !test5 || !test6 || !test7 || !test8 || !test9 || !test10 || !test11 ||
-//       !test12) {
-//     return EXIT_FAILURE;
-//   }
+  game_delete(g);
+  if (!test1 || !test2 || !test3 || !test4 || !test5 || !test6 || !test7 || !test8 || !test9 || !test10 || !test11 ||
+      !test12) {
+    return EXIT_FAILURE;
+  }
 
-//   game g2 = game_new_empty_ext(4, 4, true, true);
-//   game_set_square(g, 0, 0, S_ONE);
-//   game_set_square(g, 1, 0, S_ONE);
-//   game_set_square(g, 3, 0, S_ONE);
-//   bool test13 = !game_has_error(g, 0, 0);
-//   if (!test13) {
-//     return EXIT_FAILURE;
-//   }
-//   return EXIT_SUCCESS;
-// }
+  game g2 = game_new_empty_ext(4, 4, true, true);
+  game_set_square(g, 0, 0, S_ONE);
+  game_set_square(g, 1, 0, S_ONE);
+  game_set_square(g, 3, 0, S_ONE);
+  bool test13 = !game_has_error(g, 0, 0);
+  if (!test13) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
 
 int test_game_copy(void)
 {
@@ -429,10 +429,10 @@ int main(int argcount, char* argv[])
   if (argcount == 2) {
     if (strcmp(argv[1], "dummy") == 0) {
       test = test_dummy();
-    // } else if (strcmp(argv[1], "game_has_error") == 0) {
-    //   if (test_game_has_error()) {
-    //     test = test_game_has_error();
-    //   }
+    } else if (strcmp(argv[1], "game_has_error") == 0) {
+      if (test_game_has_error()) {
+        test = test_game_has_error();
+      }
     } else if (strcmp(argv[1], "game_copy") == 0) {
       if (test_game_copy()) {
         test = test_game_copy();
