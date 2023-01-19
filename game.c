12dbc18cfc79007c7c4bc70826c2759dbc602f6c
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2023-01-19 14:47:51 +0100

    aaaaaa

--- game.c
+++ game.c
@@ -445,8 +445,8 @@ int game_has_error(cgame g, uint i, uint j)
     int s3;
     for (int c = 0; c < g->row; c++) {
       s1 = game_get_number(g, c, j);
-      s2 = game_get_next_number(g, c, j, DOWN, 1);
-      s3 = game_get_next_number(g, c, j, DOWN, 2);
+      s2 = game_get_next_number(g, c, j, RIGHT, 1);
+      s3 = game_get_next_number(g, c, j, RIGHT, 2);
       if (s1== 0 && s2 == 0 && s3 == 0) {
         return 2;
       }
@@ -455,9 +455,11 @@ int game_has_error(cgame g, uint i, uint j)
       }
     }
     for (int c = 0; c < g->col; c++) {
+    
       s1 = game_get_number(g, c, j);
-      s2 = game_get_next_number(g, c, j, RIGHT, 1);
-      s3 = game_get_next_number(g, c, j, RIGHT, 2);
+      s2 = game_get_next_number(g, c, j, DOWN, 1);
+      s3 = game_get_next_number(g, c, j, DOWN, 2);
+      
       if (s1== 0 && s2 == 0 && s3 == 0) {
         return 2;
       }
