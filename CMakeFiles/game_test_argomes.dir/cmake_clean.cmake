6d0956b47b6b989802a657502676c3aaa9a02c84
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2023-01-20 11:11:15 +0100

    aaaaaaaa

---
+++ CMakeFiles/game_test_argomes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_test_argomes.dir/game_test_argomes.c.o"
+  "game_test_argomes"
+  "game_test_argomes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game_test_argomes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/game_test_ymaabout.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_test_ymaabout.dir/game_test_ymaabout.c.o"
+  "game_test_ymaabout"
+  "game_test_ymaabout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game_test_ymaabout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
