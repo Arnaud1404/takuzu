19c435fcebb6fc58a7fd570f3c08f8bcad8d6701
Arnaud Gomes <arnaud.gomes@etu.u-bordeaux.fr>
2023-03-01 17:59:25 +0100

    game_solve.c

--- game_solve.c
+++ game_solve.c
@@ -10,22 +10,44 @@
 
 void help(void)
 {
-  printf("> action : help\n") ;
+  printf("> action : help\n");
   printf("-press 's' to search the solution of the game\n");
   printf("-press 'c' to count the number of solution and save it\n");
 }
-
 
 int main(int argc, char* argv[])
 {
   game g;
   if (argc == 3) {
-    g = game_load(argv[1]);
-    game_solve(g);
-    game_print(g);
-    
-    char* c = argv[2];
-    game_save(g,c);
+    char* filename = argv[3];
+    g = game_load(argv[2]);
+    if (strcmp(argv[1], "-s") == 0) {
+      bool ret = game_solve(g);
+      if (!ret) {
+        return EXIT_FAILURE;
+      }
+
+      game_save(g, filename);
+    }
+
+    if (strcmp(argv[1], "-c") == 0) {
+      int n = game_nb_solutions(g);  // 0 si aucune solution
+      FILE* file_game = fopen(filename, "w");
+      if (file_game == NULL) {
+        exit(EXIT_FAILURE);
+      }
+      fprintf(file_game, "%d\n", n);
+    }
+  }
+  if (argc == 2) {
+    int n = game_nb_solutions(g);
+    if (n == 0) {
+      return EXIT_FAILURE;
+    }
+    for (int i = 0; i < n; i++) {
+      bool ret = game_solve(g);
+      game_print(g);
+    }
   }
   return EXIT_SUCCESS;
 }
