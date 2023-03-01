c86aa43644d670b5d2779a1d9924a8d2198ffc5d
Yasmine Maabout <yasmine.maabout@etu.u-bordeaux.fr>
2023-03-01 11:18:18 +0100

    aaaaaaaa

--- game_solve.c
+++ game_solve.c
@@ -12,5 +12,20 @@ void help(void)
 {
   printf("> action : help\n") ;
   printf("-press 's' to search the solution of the game\n");
-    printf("-press 'c' to count the number of solution and save it\n");
+  printf("-press 'c' to count the number of solution and save it\n");
+}
+
+
+int main(int argc, char* argv[])
+{
+  game g;
+  if (argc == 3) {
+    g = game_load(argv[1]);
+    game_solve(g);
+    game_print(g);
+    
+    char* c = argv[2];
+    game_save(g,c);
+  }
+  return EXIT_SUCCESS;
 }
